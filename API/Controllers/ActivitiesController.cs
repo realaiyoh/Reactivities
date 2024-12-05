235a7fbe05fabe9500121a3949a107364823ba98
Ali <halilovicali@proton.me>
2024-12-05 19:22:32 +0100

    Initial project setup

---
+++ API/Controllers/ActivitiesController.cs
@@ -0,0 +1,35 @@
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace API.Controllers
+{
+    public class ActivitiesController : BaseApiController
+    {
+        private readonly DataContext _context;
+        public ActivitiesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<Activity>>> GetActivities()
+        {
+            return await _context.Activities.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Activity>> GetActivity(Guid id)
+        {
+            Activity? activity = await _context.Activities.FindAsync(id);
+
+            if (activity is null)
+            {
+                throw new NullReferenceException(nameof(activity));
+            }
+
+            return activity;
+        }
+    }
+}

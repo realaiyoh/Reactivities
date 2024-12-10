c968be2923e17947e253bbcdad6106881cc1f8da
Ali <halilovicali@proton.me>
2024-12-10 14:22:02 +0100

    Add all crud operations using mediator/cqrs pattern, add services extension method

---
+++ Application/Activities/Edit.cs
@@ -0,0 +1,39 @@
+using AutoMapper;
+using Domain;
+using MediatR;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public required Activity Activity { get; set; }
+        }
+
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            private readonly IMapper _mapper;
+            public Handler(DataContext context, IMapper mapper)
+            {
+                _context = context;
+                _mapper = mapper;
+            }
+            public async Task Handle(Command request, CancellationToken cancellationToken)
+            {
+                var activity = await _context.Activities.FindAsync(request.Activity.Id);
+
+                if (activity is null)
+                {
+                    throw new NullReferenceException(nameof(activity));
+                }
+
+                _mapper.Map(request.Activity, activity);
+
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}

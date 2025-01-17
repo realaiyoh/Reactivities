4e62434d76a7733420a30ce27b5c27e0d5a6f0ef
Ali <halilovicali@proton.me>
2025-01-17 22:10:12 +0100

    Add missing styles and components

--- client/src/features/activities/dashboard/ActivityList.tsx
+++ client/src/features/activities/dashboard/ActivityList.tsx
@@ -1,16 +1,26 @@
 import { useStore } from "../../../app/stores/store";
 import { observer } from "mobx-react-lite";
 import ActivityListItem from "./ActivityListItem";
+import { Fragment } from "react/jsx-runtime";
+import { Header } from "semantic-ui-react";
 
 export default observer(function ActivityList() {
   const { activityStore } = useStore();
 
-  const { activitiesByDate } = activityStore;
+  const { groupedActivities } = activityStore;
 
   return (
     <>
-      {activitiesByDate.map((activity) => (
-        <ActivityListItem activity={activity} />
+      {groupedActivities.map(([group, activities]) => (
+        <Fragment key={group}>
+          <Header sub color="teal">
+            {group}
+          </Header>
+          {activities &&
+            activities.map((activity) => (
+              <ActivityListItem key={activity.id} activity={activity} />
+            ))}
+        </Fragment>
       ))}
     </>
   );
